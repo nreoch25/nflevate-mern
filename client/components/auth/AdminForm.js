f4590d71fa1a1ed10178c0e0e00957ab9585e842
Nigel <nigelreoch@yahoo.com>
2018-01-19 18:29:38 -0500

    Started admin form

---
+++ client/components/auth/AdminForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Field, reduxForm, propTypes } from "redux-form";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+
+const required = value => (value ? undefined : "Required");
+
+class AdminForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  handleFormSubmit(evt) {
+    evt.preventDefault();
+    console.log("GroupName", this.groupName.value);
+  }
+  render() {
+    return (
+      <div className="row vertical-offset-100">
+        <div className="col-md-4 col-md-offset-4">
+          <div className="panel panel-default box-shadow-panel">
+            <div className="panel-heading">
+              <h3 className="panel-title text-center">Admin Form</h3>
+            </div>
+            <div className="panel-body">
+              <form onSubmit={this.handleFormSubmit}>
+                <div className="form-group">
+                  <label>Group Name:</label>
+                  <input
+                    ref={ref => {
+                      this.groupName = ref;
+                    }}
+                    className="form-control"
+                    type="input"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-lg btn-success btn-block"
+                >
+                  Add Group
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminForm;
--- client/components/pages/Admin.js
+++ client/components/pages/Admin.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import AdminForm from "../auth/AdminForm";
 import requireAdmin from "../hoc/requireAdmin";
 
 class Admin extends Component {
   render() {
     return (
-      <div>
-        <h1>NFLevate admin</h1>
+      <div className="container">
+        <AdminForm />
       </div>
     );
   }
