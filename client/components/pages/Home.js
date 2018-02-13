3d46b8c1918d29288bf0d5a8125df9cfb30e10d6
Nigel <nigelreoch@yahoo.com>
2018-02-12 21:15:47 -0500

    used chunks for group cards

--- client/components/pages/Home.js
+++ client/components/pages/Home.js
@@ -13,42 +13,67 @@ class Home extends Component {
   }
   renderGroups() {
     if (this.props.groups.length > 0) {
-      // create an array for rows
-      let rowArray = [];
-      // create an array for elements within a row
-      let elementsArray = [];
-      this.props.groups.map((group, i, { length }) => {
-        elementsArray.push(
-          <div className="col-sm-12 col-md-6 no-padding-left">
-            <Card />
+      const groups = this.props.groups;
+      const chunkSize = 2;
+      const chunked = [];
+      let index = 0;
+      while (index < groups.length) {
+        chunked.push(groups.slice(index, index + chunkSize));
+        index += chunkSize;
+      }
+      let groupsArray = chunked.map((chunk, i) => {
+        let rowArray = chunk.map((group, i) => {
+          return (
+            <div key={i} className="col-sm-12 col-md-6 no-padding-left">
+              <Card />
+            </div>
+          );
+        });
+        return (
+          <div key={i} className="row margin-bottom-15">
+            {rowArray}
           </div>
         );
-        // create the row on even index so that
-        // both children can be attached to row using createElement
-        if (i % 2 !== 0) {
-          // create row and pass in two elements
-          let groupRow = React.createElement(
-            "div",
-            { className: "row margin-bottom-15" },
-            elementsArray
-          );
-          rowArray.push(groupRow);
-          // clear elements array for next row
-          elementsArray = [];
-        }
-        // if there is an even number of groups we need to
-        // check if if even number and last element and create row
-        if (i % 2 === 0 && i + 1 === length) {
-          let groupRow = React.createElement(
-            "div",
-            { className: "row" },
-            elementsArray
-          );
-          rowArray.push(groupRow);
-        }
       });
-      return rowArray;
+      return groupsArray;
     }
+    // if (this.props.groups.length > 0) {
+    //   // create an array for rows
+    //   let rowArray = [];
+    //   // create an array for elements within a row
+    //   let elementsArray = [];
+    //   this.props.groups.map((group, i, { length }) => {
+    //     elementsArray.push(
+    //       <div className="col-sm-12 col-md-6 no-padding-left">
+    //         <Card />
+    //       </div>
+    //     );
+    //     // create the row on even index so that
+    //     // both children can be attached to row using createElement
+    //     if (i % 2 !== 0) {
+    //       // create row and pass in two elements
+    //       let groupRow = React.createElement(
+    //         "div",
+    //         { className: "row margin-bottom-15" },
+    //         elementsArray
+    //       );
+    //       rowArray.push(groupRow);
+    //       // clear elements array for next row
+    //       elementsArray = [];
+    //     }
+    //     // if there is an even number of groups we need to
+    //     // check if if even number and last element and create row
+    //     if (i % 2 === 0 && i + 1 === length) {
+    //       let groupRow = React.createElement(
+    //         "div",
+    //         { className: "row" },
+    //         elementsArray
+    //       );
+    //       rowArray.push(groupRow);
+    //     }
+    //   });
+    //   return rowArray;
+    // }
   }
   render() {
     return (
