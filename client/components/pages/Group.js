bbbbfff24da79fc11fd56d360e613ab9e92017ad
Nigel <nigelreoch@yahoo.com>
2018-02-24 16:42:19 -0500

    Added send message footer

--- client/components/pages/Group.js
+++ client/components/pages/Group.js
@@ -25,80 +25,133 @@ class Group extends Component {
               <div className="card-header group-header">
                 {this.getGroupName()}
               </div>
-              <ul className="list-group list-group-flush">
+              <ul className="list-group list-group-flush line-height-75">
                 <li className="list-group-item">
                   <img
                     src={require("../../../static/images/default.png")}
-                    className="rounded-circle width-15-percent border border-primary float-left"
+                    className="rounded-circle width-10-percent border border-primary float-left"
                   />
-                  <div className="float-left width-85-percent">
+                  <div className="float-left width-90-percent">
                     <div className="padding-left-10 padding-right-10">
                       <h6 className="float-left">User Name</h6>
                       <p className="float-right">12 hours ago</p>
                     </div>
-                    <p className="padding-left-10 float-left">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <p className="padding-left-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                   </div>
                 </li>
                 <li className="list-group-item">
                   <img
                     src={require("../../../static/images/default.png")}
-                    className="rounded-circle width-15-percent border border-primary float-right"
+                    className="rounded-circle width-10-percent border border-primary float-right"
                   />
-                  <div className="float-right width-85-percent">
+                  <div className="float-right width-90-percent">
                     <div className="padding-left-10 padding-right-10">
                       <h6 className="float-right">User Name</h6>
                       <p className="float-left">12 hours ago</p>
                     </div>
-                    <p className="padding-right-10 float-right">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <p className="padding-right-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                   </div>
                 </li>
                 <li className="list-group-item">
                   <img
                     src={require("../../../static/images/default.png")}
-                    className="rounded-circle width-15-percent border border-primary float-left"
+                    className="rounded-circle width-10-percent border border-primary float-left"
                   />
-                  <div className="float-left width-85-percent">
+                  <div className="float-left width-90-percent">
                     <div className="padding-left-10 padding-right-10">
                       <h6 className="float-left">User Name</h6>
                       <p className="float-right">12 hours ago</p>
                     </div>
-                    <p className="padding-left-10 float-left">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <p className="padding-left-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                   </div>
                 </li>
                 <li className="list-group-item">
                   <img
                     src={require("../../../static/images/default.png")}
-                    className="rounded-circle width-15-percent border border-primary float-right"
+                    className="rounded-circle width-10-percent border border-primary float-right"
                   />
-                  <div className="float-right width-85-percent">
+                  <div className="float-right width-90-percent">
                     <div className="padding-left-10 padding-right-10">
                       <h6 className="float-right">User Name</h6>
                       <p className="float-left">12 hours ago</p>
                     </div>
-                    <p className="padding-right-10 float-right">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <p className="padding-right-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <img
+                    src={require("../../../static/images/default.png")}
+                    className="rounded-circle width-10-percent border border-primary float-left"
+                  />
+                  <div className="float-left width-90-percent">
+                    <div className="padding-left-10 padding-right-10">
+                      <h6 className="float-left">User Name</h6>
+                      <p className="float-right">12 hours ago</p>
+                    </div>
+                    <p className="padding-left-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <img
+                    src={require("../../../static/images/default.png")}
+                    className="rounded-circle width-10-percent border border-primary float-right"
+                  />
+                  <div className="float-right width-90-percent">
+                    <div className="padding-left-10 padding-right-10">
+                      <h6 className="float-right">User Name</h6>
+                      <p className="float-left">12 hours ago</p>
+                    </div>
+                    <p className="padding-right-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <img
+                    src={require("../../../static/images/default.png")}
+                    className="rounded-circle width-10-percent border border-primary float-left"
+                  />
+                  <div className="float-left width-90-percent">
+                    <div className="padding-left-10 padding-right-10">
+                      <h6 className="float-left">User Name</h6>
+                      <p className="float-right">12 hours ago</p>
+                    </div>
+                    <p className="padding-left-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <img
+                    src={require("../../../static/images/default.png")}
+                    className="rounded-circle width-10-percent border border-primary float-right"
+                  />
+                  <div className="float-right width-90-percent">
+                    <div className="padding-left-10 padding-right-10">
+                      <h6 className="float-right">User Name</h6>
+                      <p className="float-left">12 hours ago</p>
+                    </div>
+                    <p className="padding-right-10 clear-both line-height-125">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                   </div>
                 </li>
               </ul>
+              <div className="chat-message-placeholder" />
             </div>
           </div>
           <div className="col-sm-4">
@@ -107,6 +160,16 @@ class Group extends Component {
             </div>
           </div>
         </div>
+        <footer className="chat-footer-send">
+          <div className="container">
+            <div className="input-group chat-message">
+              <input className="form-control chat-input" />
+              <span className="input-group-btn">
+                <button className="chat-btn">Send Message</button>
+              </span>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
