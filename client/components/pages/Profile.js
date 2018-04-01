6e26605418a011ad4d47121d41bc40e771a179f0
Nigel <nigelreoch@yahoo.com>
2018-04-01 12:39:40 -0400

    Added Profile page and static information

--- client/components/pages/Profile.js
+++ client/components/pages/Profile.js
@@ -1,13 +1,69 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import requireAuth from "../hoc/requireAuth";
 
 class Profile extends Component {
+  renderProfileLinks() {
+    if (this.props.user.username === this.props.match.params.user) {
+      return (
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Update Profile Information</li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Send Friend Request</li>
+          <li className="list-group-item">Send Private Message</li>
+        </ul>
+      );
+    }
+  }
   render() {
     return (
-      <div>
-        <h1>Profile Page for {this.props.match.params.user}</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4 no-padding-left">
+            <div className="card">
+              <div className="card-header group-header">
+                {this.props.match.params.user} Profile
+              </div>
+              <img
+                className="width-100"
+                src={require("../../../static/images/profile.jpeg")}
+              />
+              {this.renderProfileLinks()}
+            </div>
+          </div>
+          <div className="col-sm-8 no-padding-left">
+            <div className="card">
+              <div className="card-header group-header">
+                {this.props.match.params.user} Information
+              </div>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <span className="font-weight-bold">Favourite Team:</span>{" "}
+                  Dallas Cowboys
+                </li>
+                <li className="list-group-item">
+                  <span className="font-weight-bold">
+                    Favourite Player:
+                  </span>{" "}
+                  Tony Romo
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user
+  };
+}
+
+export default requireAuth(connect(mapStateToProps, null)(Profile));
