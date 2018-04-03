14e9d863295073423333c83ca3a3dba40b8bc275
Nigel <nigelreoch@yahoo.com>
2018-04-03 18:11:27 -0400

    Added a Dockerfile for the fullstack app

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+
+FROM node:8.1.1
+
+RUN npm install -g yarn
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app
+COPY yarn.lock /usr/src/app
+
+RUN yarn install --pure-lockfile
+COPY . /usr/src/app
+
+EXPOSE 8000
+CMD ["npm", "run", "bs"]
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./AuthReducer";
 import groupsReducer from "./GroupsReducer";
-import profileReducer from "./profileReducer";
+import profileReducer from "./ProfileReducer";
 
 export default combineReducers({
   form: formReducer,
